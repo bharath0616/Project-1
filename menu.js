4e84a3ea77cb059627314cde48a41c995c9c250b
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-09-14 10:44:23 +0530

    cart

--- menu.js
+++ menu.js
@@ -69,17 +69,44 @@ $(document).ready(function() {
     })
     
   });
-
+  
   $(document).ready(function(){
-    var i=0;
-    $(".cartbtn").click(function(){
+    function increment(){
+        var i=0;
+        $(".cartbtn").click(function(){
         
-        $(".quantity").text(i+=1);
-    })
+            $(".quantity").text(i+=1);
+            $(".cpbtn").show();
+            
+        })
 
+    }
+    increment();
     $(".cartbtn").click(function(){
         var elem=$(this).siblings("img, p, h3").clone();        
         $(".cartitems").append(elem);
     });
     
+  });
+
+  $(document).ready(function(){
+    $(".clear-cart").click(function(){
+        $(".cartitems").empty();
+        $(".cartpage").slideUp(500);
+        $(".cpbtn").hide();
+        $(".quantity").text("0")
+        increment();
+        
+    })
+    
+  });
+
+  $(document).ready(function(){
+    function checkContent() {
+        if ($(".cartitems").empty ) {
+            // Content is present, show the cpbtn section
+            $(".cpbtn").hide()
+        } 
+    }
+    checkContent()
   });
