1141fbcfa6e05ce5fb318322641492189d6eb12e
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-09-27 16:04:42 +0530

    x

--- menu.js
+++ menu.js
@@ -70,55 +70,49 @@ $(document).ready(function() {
     
   });
   
-  $(document).ready(function () {
-    var i = 1;
-    var costPerProduct = Number($(".cartitems3 p").text()); // Initialize cost per product from h1
-
-    $(".plus").click(function () {
-        i += 1;
-        $(".cartitems2 span").text(i);
-
-        // Calculate the total cost by multiplying the cost per product by the number of products
-        var totalCost = costPerProduct * i;
-        $(".cartitems3 p").text(totalCost);
-    });
-
-    $(".minus").click(function () {
-        if(i>0){
-        i -= 1;
-        $(".cartitems2 span").text(i);
-
-        // Calculate the total cost by multiplying the cost per product by the number of products
-        var totalCost = costPerProduct * i;
-        $(".cartitems3 p").text(totalCost);
-        }
-});
-});
-
-$(document).ready(function () {
-    $(".cartbtn").click(function () {
-        // Get the item details
-        var itemName = $("#adb").text();
-        var itemPrice = parseInt($(".list p").text()); // Assuming price is an integer
-
-        // Create a new cart item
-        var cartItem = $("<div class='cartitem'></div>");
-        var cartItems = $("<div class='cartitems'></div>");
-        var cartItems2 = $("<div class='cartitems2'></div>");
-        var cartItems3 = $("<div class='cartitems3'></div>");
-
-        // Set the cart item details
-        cartItems.append("<h3>" + itemName + "</h3>");
-        cartItems2.append("<button class='plus'>+</button>");
-        cartItems2.append("<span>1</span>");
-        cartItems2.append("<button class='minus'>-</button>");
-        cartItems3.append("<p>" + itemPrice + "</p>");
-
-        // Append the cart item to the cart section
-        cartItem.append(cartItems);
-        cartItem.append(cartItems2);
-        cartItem.append(cartItems3);
-        $(".cartpage").append(cartItem);
-    });
-});
   
+  $(document).ready(function() {
+    $(".cartbtn").click(function() {
+      // Get the product details from the clicked item
+      var $parentDiv = $(this).closest(".list");
+      var productName = $parentDiv.find("h3").text();
+      var productPrice = parseFloat($parentDiv.find("span").text()); // Parse the price as a float
+
+      // Create a new cart item div and set its properties
+      var $cartItem = $("<div>").addClass("cartitem");
+      var $cartItems = $("<div>").addClass("cartitems").append($("<h3>").text(productName));
+      var $cartItems2 = $("<div>").addClass("cartitems2");
+      var $plusButton = $("<button>").addClass("plus").text("+");
+      var $quantitySpan = $("<span>").text("1");
+      var $minusButton = $("<button>").addClass("minus").text("-");
+      $cartItems2.append($plusButton, $quantitySpan, $minusButton);
+      var $cartItems3 = $("<div>").addClass("cartitems3").append($("<span>").text(productPrice));
+
+      // Append the cart item to the cart container
+      $cartItem.append($cartItems, $cartItems2, $cartItems3);
+      $(".cartpage").append($cartItem);
+
+      // Add event handlers for the plus and minus buttons within the cart item
+      $plusButton.click(function() {
+        var quantity = parseInt($quantitySpan.text());
+        quantity += 1;
+        $quantitySpan.text(quantity);
+        updateTotalCost();
+      });
+
+      $minusButton.click(function() {
+        var quantity = parseInt($quantitySpan.text());
+        if (quantity > 0) {
+          quantity -= 1;
+          $quantitySpan.text(quantity);
+          updateTotalCost();
+        }
+      });
+
+      function updateTotalCost() {
+        var quantity = parseInt($quantitySpan.text());
+        var totalCost = productPrice * quantity;
+        $cartItems3.find("span").text(totalCost);
+      }
+    });
+  });
