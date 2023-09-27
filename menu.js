3ea8136e84639fe1929bb9a646a803369c5c4872
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-09-27 21:27:29 +0530

    cart update

--- menu.js
+++ menu.js
@@ -62,16 +62,24 @@ $(document).ready(function() {
 
   $(document).ready(function(){
     $(".cart").click(function(){
-        $(".cartpage").addClass("cart-display"); 
+        $(".cartpage").addClass(".cartpage show"); 
+        
+        
     })
     $("h8").click(function(){
-        $(".cartpage").removeClass("cart-display"); 
+        
+        $(".cartpage").removeClass(".cartpage show");
+        $(".cartpage").addClass(".cart-hide");  
+       
     })
     
   });
   
-  
+
+   
+ 
   $(document).ready(function() {
+    $(".cpbtn").hide();
     $(".cartbtn").click(function() {
       // Get the product details from the clicked item
       var $parentDiv = $(this).closest(".list");
@@ -80,6 +88,7 @@ $(document).ready(function() {
 
       // Create a new cart item div and set its properties
       var $cartItem = $("<div>").addClass("cartitem");
+      var $cpbtn = $(".cpbtn")
       var $cartItems = $("<div>").addClass("cartitems").append($("<h3>").text(productName));
       var $cartItems2 = $("<div>").addClass("cartitems2");
       var $plusButton = $("<button>").addClass("plus").text("+");
@@ -91,6 +100,8 @@ $(document).ready(function() {
       // Append the cart item to the cart container
       $cartItem.append($cartItems, $cartItems2, $cartItems3);
       $(".cartpage").append($cartItem);
+      $(".cartpage").append($cpbtn);
+      $cpbtn.show();
 
       // Add event handlers for the plus and minus buttons within the cart item
       $plusButton.click(function() {
@@ -107,7 +118,14 @@ $(document).ready(function() {
           $quantitySpan.text(quantity);
           updateTotalCost();
         }
+
+        // Check if quantity is zero and remove the cart item
+        if (quantity === 0) {
+          $cartItem.remove();
+        }
+        
       });
+      
 
       function updateTotalCost() {
         var quantity = parseInt($quantitySpan.text());
@@ -115,4 +133,10 @@ $(document).ready(function() {
         $cartItems3.find("span").text(totalCost);
       }
     });
-  });
+    
+    $(".clear-cart").click(function(){
+     $(".cartitem").remove();
+    })
+});
+
+
