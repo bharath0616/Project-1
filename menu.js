6b832ef0e5656446940c2b3a0c6f5e846b0ea873
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-09-30 13:54:25 +0530

    cart

--- menu.js
+++ menu.js
@@ -85,24 +85,36 @@ $(document).ready(function() {
       var $parentDiv = $(this).closest(".list");
       var productName = $parentDiv.find("h3").text();
       var productPrice = parseFloat($parentDiv.find("span").text()); // Parse the price as a float
-
+      var productImageSrc = $parentDiv.find("img").attr("src"); // Get the image source
+  
       // Create a new cart item div and set its properties
       var $cartItem = $("<div>").addClass("cartitem");
-      var $cpbtn = $(".cpbtn")
-      var $cartItems = $("<div>").addClass("cartitems").append($("<h3>").text(productName));
+      var $cpbtn = $(".cpbtn");
+  
+      // Create an img element with the product image
+      var $productImage = $("<img>").attr("src", productImageSrc);
+  
       var $cartItems2 = $("<div>").addClass("cartitems2");
       var $plusButton = $("<button>").addClass("plus").text("+");
       var $quantitySpan = $("<span>").text("1");
       var $minusButton = $("<button>").addClass("minus").text("-");
-      $cartItems2.append($plusButton, $quantitySpan, $minusButton);
-      var $cartItems3 = $("<div>").addClass("cartitems3").append($("<span>").text(productPrice));
 
-      // Append the cart item to the cart container
+      $cartItems2.append($plusButton);
+      $cartItems2.append($quantitySpan);
+      $cartItems2.append($minusButton);
+  
+      // Create a div for cart items that contains both the product name and image
+      var $cartItems = $("<div>").addClass("cartitems");
+      $cartItems.append($("<h3>").text(productName));
+      $cartItems.append($productImage);
+  
+      var $cartItems3 = $("<div>").addClass("cartitems3").append($("<span>").text(productPrice));
+  
       $cartItem.append($cartItems, $cartItems2, $cartItems3);
       $(".cartpage").append($cartItem);
       $(".cartpage").append($cpbtn);
       $cpbtn.show();
-
+  
       // Add event handlers for the plus and minus buttons within the cart item
       $plusButton.click(function() {
         var quantity = parseInt($quantitySpan.text());
@@ -110,7 +122,7 @@ $(document).ready(function() {
         $quantitySpan.text(quantity);
         updateTotalCost();
       });
-
+  
       $minusButton.click(function() {
         var quantity = parseInt($quantitySpan.text());
         if (quantity > 0) {
@@ -118,25 +130,24 @@ $(document).ready(function() {
           $quantitySpan.text(quantity);
           updateTotalCost();
         }
-
+  
         // Check if quantity is zero and remove the cart item
         if (quantity === 0) {
           $cartItem.remove();
         }
-        
       });
-      
-
+  
       function updateTotalCost() {
         var quantity = parseInt($quantitySpan.text());
         var totalCost = productPrice * quantity;
         $cartItems3.find("span").text(totalCost);
       }
     });
+  });
+  
     
     $(".clear-cart").click(function(){
      $(".cartitem").remove();
-    })
-});
+    });
 
 
