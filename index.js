89e53f85e8bf0482d7ec60f30462757c5c2ce329
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-10-22 17:58:58 +0530

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index.html', function(req, res, next) {
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
