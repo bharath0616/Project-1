c6f41eb8b327c38b67605b553c3b33fbbd7c2ea2
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-10-22 12:40:55 +0530

    rearrange

---
+++ public/index.js
@@ -0,0 +1,87 @@
+const http =require('http')
+const app=require('./app');
+app.set('port',3000);
+const server = http.createServer(app);
+
+server.listen(3000);
+
+
+$(document).ready(function(){
+    $(".bar").click(function(){
+        $(".dropdownmenu").show(1000);
+        $(".bar").slideUp(200);
+        $(".x").slideDown(200);
+
+    });
+});
+$(document).ready(function(){
+    $(".x").click(function(){
+        $(".dropdownmenu").hide(1000);
+        $(".x").slideUp(200);   
+        $(".bar").slideDown(200);            
+    });
+});
+$(document).ready(function(){
+    $(".dropdownmenu a").click(function(){
+        $(".dropdownmenu").hide(1000);
+        $(".x").slideUp(200);   
+        $(".bar").slideDown(200); 
+    }) 
+});
+$(document).ready(function(){
+    $(".dropdownmenu ").click(function(){
+        $(".dropdownmenu").hide(1000);
+        $(".x").slideUp(200);   
+        $(".bar").slideDown(200); 
+    }) 
+});
+
+/* $(document).ready(function(){
+    $(window).scroll(function(){
+        $(".nv").addClass("sticky")
+    })
+    
+}); */
+
+$(document).ready(function () {
+    $(".contactus").click(function(){
+        $(".screen").fadeIn(1000);
+        $(".screen").addClass("overlay")
+        $(".nv").addClass("filter")
+        $("#aboutus").addClass("filter")
+        $(".corousel").addClass("filter")
+        $(".footer").addClass("filter")
+
+    })
+});
+
+$(document).ready(function () {
+    $(".app-form-button1").click(function(){
+        $(".screen").hide();
+        $(".nv").removeClass("filter")
+        $("#aboutus").removeClass("filter")
+        $(".corousel").removeClass("filter")
+        $(".footer").removeClass("filter")
+    
+    })
+});
+
+$(document).ready(function(){
+    $(".app-form-button2").click(function(){
+        $(".thankyou").addClass("overlay");
+        $(".screen").hide();
+        $(".thankyou").slideDown(500).fadeOut(5000);
+        $(".nv").removeClass("filter")
+        $("#aboutus").removeClass("filter")
+        $(".corousel").removeClass("filter")
+        $(".footer").removeClass("filter")
+    })
+    
+});
+
+
+
+
+
+
+
