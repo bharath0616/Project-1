fb9f7ac03faa1aecb14c741425d5e8e52c614d26
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2023-12-12 23:16:32 +0530

    rearranged views

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
+app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/index.html', function(req, res, next) {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + 'views/index.html');
 });
 
 app.listen(port);
